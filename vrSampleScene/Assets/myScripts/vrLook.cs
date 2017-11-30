74c75f85bb7c906b91d9912813e19798fab6b1c1
Furkan Ozdemir <furkanozdmrr@hotmail.com>
2017-11-30 22:41:46 +0300

    First

---
+++ vrSampleScene/Assets/myScripts/vrLook.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class vrLook : MonoBehaviour {
+    private bool isCreate = true;
+    public void VrLookAtMe(bool m)
+    {
+       
+
+            Debug.Log("Bakıyorsun");
+        StartCoroutine(lookAndFall());
+    
+}
+
+    public IEnumerator lookAndFall()
+    {
+            yield return new WaitForSeconds(5f);
+            gameObject.GetComponent<Rigidbody>().useGravity = true;
+
+
+            }
+    }
+}
